fe60181208b03e0a557dea1ce3efafcd08fcd3db
Kinzaqirat <159807119+Kinzaqirat@users.noreply.github.com>
2024-03-05 01:00:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var name_ = "Kinza Saeed";
+console.log(name_.toLowerCase());
+console.log(name_.toUpperCase());
+console.log(name_.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let name_ = "Kinza Saeed";
+console.log(name_.toLowerCase());
+console.log(name_.toUpperCase());
+console.log(name_.replace(/\b\w/g, (char) => char.toUpperCase()));
